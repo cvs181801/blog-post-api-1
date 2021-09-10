36f8c13977e636150b5e2af426159acaf15ac210
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-09 18:04:55 -0700

    add in the rendering for the data we just fetched w POST method

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const body = document.querySelector("body");
 const form = document.querySelector(".form");
 const formTitle = document.getElementById("formTitle");
 const formBody = document.getElementById("formBody");
+let myPostTitle = document.createElement("h2");
+let myPostBody = document.createElement("p");
 
 //create a way to get the blog posts from the API, and display them on the screen.
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        const postsArr = data.slice(0, 5)
+// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+//     .then(res => res.json())
+//     .then(data => {
+//         const postsArr = data.slice(0, 5)
     
-        postsArr.forEach((item, index) => {
-            let horizontalLine = document.createElement('hr');
-            let postTitle = document.createElement('h2');
-            postTitle.textContent = `title: ${item.title}`
-            body.append(postTitle);
+//         postsArr.forEach((item, index) => {
+//             let horizontalLine = document.createElement('hr');
+//             let postTitle = document.createElement('h2');
+//             postTitle.textContent = `title: ${item.title}`
+//             body.append(postTitle);
             
-            let postBody = document.createElement('p');
-            postBody.textContent = `body: ${item.body}`;
-            body.append(postBody);
-            body.append(horizontalLine);
-        })
-    })
+//             let postBody = document.createElement('p');
+//             postBody.textContent = `body: ${item.body}`;
+//             body.append(postBody);
+//             body.append(horizontalLine);
+//         })
+//     })
 
 let Post = class {
     constructor(title, body) {
@@ -34,26 +36,26 @@ let Post = class {
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     let myPost = new Post(formTitle.value, formBody.value);
-    //console.log(myPost);
-    // let myPostTitle = document.createElement("h2");
-    // myPostTitle.textContent = `${myPost.title}`;
-    // body.append(myPostTitle);
-
-    // let myPostBody = document.createElement("p");
-    // myPostBody.textContent = `${myPost.body}`;
-    // body.append(myPostBody);
+    console.log(myPost);
 
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
-        method: 'POST', 
+        method: "POST", 
         body: JSON.stringify({
-            title: formTitle.value,
-            body: formBody.value,
+            title: "some title",
+            body: "some body",
             completed: false
         }),
         headers: {
-            "Content-Type": "application/json; charset=UTF8"
+            "Content-Type": "application/json; charset=UTF-8"
         }
     })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then((data) => {
+            myPostTitle.textContent = `${data.title}`;
+            body.append(myPostTitle);
+
+            myPostBody.textContent = `${data.body}`;
+            body.append(myPostBody);
+        })
 })    
+
